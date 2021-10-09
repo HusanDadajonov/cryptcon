50802b875f5bad6c93f331528ba8aae9ceb6f488
am1rcek <xusandadajonov4385@gmail.com>
2021-10-09 14:18:03 -0700

    dsc

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let elArrow = document.querySelector(".top");
 let elMenuPage = document.querySelector(".menu-page");
 let elHeaderMenu = document.querySelector(".header__menu");
+let elLinethird =document.querySelector(".line2");
+let elLineOne =document.querySelector(".line1");
+let elLineThree =document.querySelector(".line3");
 window.addEventListener("scroll",function(){
     header = document.querySelector("header");
     header.classList.toggle("header-bg",window.scrollY > 10);
@@ -8,4 +11,7 @@ window.addEventListener("scroll",function(){
 })
 elHeaderMenu.addEventListener("click",function(){
     elMenuPage.classList.toggle("open-menu");
+    elLinethird.classList.toggle("remove-line");
+    elLineOne.classList.toggle("rotate-line1")
+    elLineThree.classList.toggle("rotate-line3")
 })
