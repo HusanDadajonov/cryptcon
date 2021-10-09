4f1bf75fc2c772d9e44f59072ddaa03d77dab354
am1rcek <xusandadajonov4385@gmail.com>
2021-10-09 12:51:37 -0700

    awd

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 let elArrow = document.querySelector(".top");
+let elMenuPage = document.querySelector(".menu-page");
+let elHeaderMenu = document.querySelector(".header__menu");
 window.addEventListener("scroll",function(){
     header = document.querySelector("header");
     header.classList.toggle("header-bg",window.scrollY > 10);
     elArrow.classList.toggle("show-arrow",window.scrollY > 10);
+})
+elHeaderMenu.addEventListener("click",function(){
+    elMenuPage.classList.toggle("open-menu");
 })
